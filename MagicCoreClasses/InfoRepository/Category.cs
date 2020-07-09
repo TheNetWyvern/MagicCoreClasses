cfc64e2a0d21b0063155faba0cc2ba3f4bb97806
Дмитрий Никитин <thenetwyvern@gmail.com>
2020-07-09 23:47:51 +0300

    added category class

---
+++ MagicCoreClasses/InfoRepository/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCoreClasses
+{
+    public class Category
+    {
+        //ID категории
+        public int id { get; set; }
+        //ID родительской категории
+        public int parentId { get; set; }
+        //Имя категории
+        public string name { get; set; }
+        //Описание категории
+        public string description { get; set; }
+
+
+    }
+}
